5268b08b7adc82dcc0a07d71f395b54e2a57cda3
stupidmxx <stupidmxx@gmail.com>
2014-05-13 22:36:22 +0800

    Finished first version for assignment of week 1.

--- profun-coursera/recfun/src/main/scala/recfun/Main.scala
+++ profun-coursera/recfun/src/main/scala/recfun/Main.scala
@@ -1,4 +1,5 @@
 package recfun
+
 import common._
 
 object Main {
@@ -14,15 +15,41 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c > r || c < 0 || r < 0) throw new IllegalArgumentException("Invalid input. c=" + c + ", r=" + r)
+    if (c == r || c == 0 || r == 0) return 1
+    else return pascal(c - 1, r) + pascal(c, r - 1)
+
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def recBalance(str: List[Char], count: Int): Boolean = {
+      if (str.isEmpty) return count == 0
+      if (count < 0) return false
+      val diff = if (str.head == '(') 1 else if (str.head == ')') -1 else 0
+      return recBalance(str.tail, count + diff)
+    }
+    return recBalance(chars, 0)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    // println("Money=" + money + ", coins=" + coins)
+    if (money == 0) {
+      // println("solution found.");
+      return 1
+    }
+    if (coins.isEmpty) return 0
+    var result = 0
+    val tail = coins.tail
+    for (i <- 0.to(money, coins.head)) {
+      result += countChange(money - i, tail)
+    }
+    return result
+  }
 }
